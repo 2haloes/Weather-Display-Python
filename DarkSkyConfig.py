9573429fb5c75d35fc943f6fc337ea76b86bae0f
2haloes <rhyshenson@gmail.com>
2018-04-23 08:28:43 +0100

    Adding files to remote

---
+++ DarkSkyConfig.py
@@ -0,0 +1,13 @@
+# This file holds the values to create a usable API call within the main program
+# Location Data (A web service can help find these if required)
+Lat = "50.852836" #Insert Latitude here
+Long = "0.558773" #Insert Longitude here
+# The following options are avliable for the units
+# auto (Default) - Picks units based on location, unless the units differ from the location, it's recommended to keep to this default
+# ca - Uses SI units but wind speed & wind gust are in kilometers per hour (Useful if edited to include those values)
+# uk2 - Uses SI units however distence is measured in miles and speed is measured in miles per hour
+# us - Uses Imperial units (°F)
+# si - Uses SI units (°C)
+Units = "auto"
+# API key, keep this hidden, to aquire this, you need to sign up to a Dev account on the DarkSky website (This program should do the rest of the work)
+ApiKey = "a29d6bfa474ca4788425d6e0303f472e"
---
+++ Whether_display.py
@@ -0,0 +1,102 @@
+import glob
+import json
+import requests
+import sched
+import time
+import tkinter as tk
+from tkinter import ttk, PhotoImage, Frame
+import webbrowser
+# This loads the DarkSkyConfig.py values
+import DarkSkyConfig
+
+class Window(ttk.Frame):
+
+    def __init__(self, master=None):
+        ttk.Frame.__init__(self, master)               
+        self.master = master
+        self.loopcount = 24
+        # Variables to use
+
+        # Current variables
+        self.timeText = tk.StringVar()
+        self.timeText.set("00:00AM")
+        self.currentPhotoImage = PhotoImage(file="images/cloudy.png")
+        self.currentPhotoImageText = tk.StringVar()
+        self.currentPhotoImageText.set("Null")
+        self.currentTempText = tk.StringVar()
+        self.currentTempText.set("0.0")
+        self.apparentTempText = tk.StringVar()
+        self.apparentTempText.set("(0.0)")
+        self.currentSummeryText = tk.StringVar()
+        self.currentSummeryText.set("Unknown")
+
+        # Widgets
+
+        # Current widgets
+        self.nowFrame = Frame(root)
+        self.daysFrame = Frame(root)
+        self.day0Frame = Frame(root)
+        self.day1Frame = Frame(root)
+        self.day2Frame = Frame(root)
+        self.day3Frame = Frame(root)
+        self.day4Frame = Frame(root)
+        # textvariable automatically updates with the StringVar() it's assigned to
+        self.timeLabel = ttk.Label(self.nowFrame, textvariable=self.timeText, font=("arial", 40, "bold")).grid()
+        # Setting the grid after creating the label keeps the refence open allowing the image to be changed later
+        self.currentIcon = ttk.Label(self.nowFrame, image=self.currentPhotoImage)
+        self.currentIcon.grid(column=0, row=1)
+        self.currentIconText = ttk.Label(self.nowFrame, textvariable=self.currentPhotoImageText, font=("arial", 21, "bold")).grid(column=0, row=2)
+        self.currentTemp = ttk.Label(self.nowFrame, textvariable=self.currentTempText, font=("arial", 58, "bold")).grid(column=0, row=3)
+        self.apparentTemp = ttk.Label(self.nowFrame, textvariable=self.apparentTempText, font=("arial", 12)).grid(column=0, row=4)
+        self.currentSummery = ttk.Label(self.nowFrame, textvariable=self.currentSummeryText, font=("arial", 10), wraplength=300).grid(column=0, row=5)
+        self.poweredBy = ttk.Label(self.nowFrame, text="Powered By DarkSky", font=("arial", 8), foreground="white", cursor="hand2")
+        self.poweredBy.grid(column=0, row=6)
+        self.poweredBy.bind("<Button-1>", openApiLink)
+
+        # Setting the frames
+        self.nowFrame.grid()
+
+
+        
+      
+
+def DisUpdate():
+    if app.loopcount == 24:
+        VarSet()
+    app.loopcount += 1
+    # This displays the local time in a 12 hour format
+    app.timeText.set(time.strftime("%I:%M%p"))
+    root.update_idletasks()
+    #This loops the current function after 5 seconds
+    root.after(5000, DisUpdate)
+    return()
+
+def VarSet():
+    app.loopcount = -1
+    # Current values
+    # Update the image by updating the variable then the label
+    app.currentPhotoImage = PhotoImage(file="images/wind.png")
+    app.currentIcon.configure(image=app.currentPhotoImage)
+    app.currentTempText.set("888.88")
+    app.apparentTempText.set("(888.88)")
+    app.currentPhotoImageText.set("Not null")
+    app.currentSummeryText.set("There may be rain within the hour, more details will be coming soon, please wait")
+
+def openApiLink(event):
+    webbrowser.open("https://darksky.net/poweredby/")
+
+# Creating the window
+root = tk.Tk()
+app = Window(root)
+app.master.title("Weather Display")
+app.master.geometry("800x480")
+root.minsize(800,480)
+# Loads the DisUpdate function after loading the window
+root.after(5000, DisUpdate)
+root.mainloop()
+
+
+
+#content = json.loads(requests.get("https://api.darksky.net/forecast/a29d6bfa474ca4788425d6e0303f472e/50.852836,0.558773?units=si").text)
+#content = json.loads(open("darksky_file.json").read())
+#print(content["minutely"]["icon"])
